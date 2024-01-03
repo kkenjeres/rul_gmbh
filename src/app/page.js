4c121779a562106b4d0ae88d706873c69fd91d04
kkenjeres <kkenjeres96@gmail.com>
2024-01-03 22:41:56 +0100

    Leaflet Map applied succesfully !

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,14 @@
 import Contact from "@/components/Contact";
 import Principles from "@/components/Principles";
+import Location from "@/components/Location";
 import { ServicesList } from "@/components/ServiceList.jsx";
 import { services } from "../data/ServicesData.js";
+
 export default function Home() {
   return (
     <main className="bg-[#F5F5F7]">
       <Principles />
+      <Location />
       <ServicesList services={services} />
       <Contact />
     </main>
---
+++ src/components/Map.jsx
@@ -0,0 +1,41 @@
+"use client";
+import { useState } from "react";
+import "leaflet/dist/leaflet.css";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import L from "leaflet";
+
+export default function Map() {
+  const [geoData, setGeoData] = useState({
+    lat: 48.77496429303292,
+    lng: 9.174478768916563,
+  });
+
+  const center = [geoData.lat, geoData.lng];
+
+  const customIcon = new L.Icon({
+    iconUrl: "/images/pin.svg",
+    iconSize: [50, 50],
+    iconAnchor: [25, 50], // Обновите это для корректного отображения икон
+  });
+
+  return (
+      
+
+      <MapContainer
+        className="z-0 m-auto rounded-lg shadow-xl"
+        center={center}
+        zoom={20}
+        style={{ height: "400px", width: "80%" }}
+        attributionControl={false}
+      >
+        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+        {geoData.lat && geoData.lng && (
+          <Marker position={[geoData.lat, geoData.lng]} icon={customIcon}>
+            <Popup className="rounded-lg">
+              <p className="font-spaceGrotesk text-lg">Königstraße 62 RUL</p>
+            </Popup>
+          </Marker>
+        )}
+      </MapContainer>
+  );
+}
