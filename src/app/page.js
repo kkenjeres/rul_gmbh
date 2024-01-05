8d09cfa4b0a18f929331cc587423ec532b426a3f
kkenjeres <kkenjeres96@gmail.com>
2024-01-05 15:56:47 +0100

    Hero section

--- src/app/page.js
+++ src/app/page.js
@@ -3,10 +3,12 @@ import Principles from "@/components/Principles";
 import Location from "@/components/Location";
 import { ServicesList } from "@/components/ServiceList.jsx";
 import { services } from "../data/ServicesData.js";
+import Hero from "@/components/Hero.jsx";
 
 export default function Home() {
   return (
     <main className="bg-[#F5F5F7]">
+      <Hero/>
       <Principles />
       <Location />
       <ServicesList services={services} />
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
-
-export const Hero = () => {
+import React from "react";
+import Image from "next/image";
+import HeroImage from "../../public/images/hero.png";
+const Hero = () => {
   return (
-    <section>
-      
+    <section className="h-screen w-full py-40">
+      <div className="m-auto md:flex h-full w-[80%] gap-4">
+        <div className="flex h-full md:w-[40%] justify-center rounded-xl border  border-black">
+          <div className="m-auto flex h-[60%] w-[80%] flex-col items-center justify-between ">
+            <h1 className="text-[60px]">Herzlich willkommen bei R.U.L. GmbH</h1>
+            <p className="text-[30px]">
+              Ihr Partner für Reinigung & Gebäudemanagement aus Stuttgartt
+            </p>
+          </div>
+        </div>
+        <Image
+          src={HeroImage}
+          alt="Hel"
+          objectFit="contain"
+          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+          className="h-full  md:w-[60%] rounded-xl border border-black"
+        ></Image>
+      </div>
     </section>
-  )
-}
+  );
+};
+export default Hero;
