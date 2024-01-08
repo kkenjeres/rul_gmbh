4f95110ed8b7a980bb0390260e94a25fee807269
kkenjeres <kkenjeres96@gmail.com>
2024-01-08 23:53:18 +0100

    hero section some changes style

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -22,7 +22,7 @@ const Hero = () => {
             alt="Hel"
             objectFit="contain"
             sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-            className="h-full  rounded-xl   "
+            className="h-full  rounded-xl   p-10"
           ></Image>
         </div>
       </div>
